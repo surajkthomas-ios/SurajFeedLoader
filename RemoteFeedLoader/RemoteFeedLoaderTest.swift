3651a228be23827ff8fee9d42f005151bc4bf884
suraj Thomas <ksurajt@gmail.com>
2022-08-24 17:49:23 +0530

    Add RemoteFeedLoader Result type with success/failure cases

--- RemoteFeedLoader/RemoteFeedLoaderTest.swift
+++ RemoteFeedLoader/RemoteFeedLoaderTest.swift
@@ -67,12 +67,11 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private func expect(_ sut: RemoteFeedLoader, toCompleteWithError error: RemoteFeedLoader.Error, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
-        var capturedErrors = [RemoteFeedLoader.Error]()
-        sut.load { capturedErrors.append($0) }
+        var capturedResult = [RemoteFeedLoader.Result]()
+        sut.load { capturedResult.append($0) }
         
         action()
-        
-        XCTAssertEqual(capturedErrors, [error], file: file, line: line)
+        XCTAssertEqual(capturedResult, [.failure(error)], file: file, line: line)
     }
 
     private class HTTPClientSpy: HTTPClient {
